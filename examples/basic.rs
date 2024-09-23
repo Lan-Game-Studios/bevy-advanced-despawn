04b126216bb42fd204f8df3d62f4fd348a8e68b6
Paul Jochims <paul.jochims@tecbeast.gmbh>
2024-09-23 12:36:16 +0200

    Add despawn with event

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,19 +1,30 @@
-use bevy_advanced_despawn::{despawn_after_timer, TimerPlugin};
-use bevy_app::{App, Startup};
+use bevy_advanced_despawn::{
+    despawn_after_timer,
+    despawn_with_event::{despawn_with_event, DespawnWithEvent},
+    TimerPlugin,
+};
+use bevy_app::{App, Startup, Update};
+use bevy_ecs::event::Event;
 use bevy_ecs::{prelude::Component, system::Commands};
 use bevy_time::{Timer, TimerMode};
 
 #[derive(Component)]
 struct A;
 
+#[derive(Event)]
+struct TestEvent;
+
 fn main() {
     App::new()
         .add_plugins(TimerPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, despawn_with_event::<TestEvent>)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     let timer = Timer::from_seconds(1.0, TimerMode::Once);
     commands.spawn((A, despawn_after_timer::DespawnAfterTimer(timer)));
+
+    commands.spawn((A, DespawnWithEvent::<TestEvent>::default()));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod despawn_after_timer;
-mod prelude;
+pub mod despawn_with_event;
+pub mod prelude;
 
 use bevy_app::{App, Plugin, Update};
 use bevy_ecs::{entity::Entity, event::Event, prelude::Component};
@@ -49,19 +50,6 @@ pub struct DespawnAfterFrames(pub usize);
 /// TODO make test
 /// TODO make example
 pub struct DespawnWith(pub Entity);
-
-/// despawn the entity when an event E is written
-/// TODO make the system to process this
-/// TODO make test
-/// TODO make example
-pub struct DespawnWithEvent<E: Event>(pub Entity, PhantomData<E>);
-
-/// TODO check if this is usefull in example
-impl<E: Event> From<Entity> for DespawnWithEvent<E> {
-    fn from(value: Entity) -> Self {
-        Self(value, PhantomData::<E>)
-    }
-}
 
 /// despawn the entity when an event E is written and
 /// the event impl Into<Entity> (custom pub trait better?)
